e5bc45dcc61f904d9553b8bc5175305379c2950c
Phillip Phan <colPhilPhan@gmail.com>
2026-02-27 14:41:25 -0600

    Generic Jackson Depricated, JacksonJson updated

--- src/main/java/com/phillip/sportsdashboard/config/RedisConfig.java
+++ src/main/java/com/phillip/sportsdashboard/config/RedisConfig.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.client.RestTemplate;
@@ -23,14 +23,17 @@ public class RedisConfig {
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        JacksonJsonRedisSerializer<Object> serializer = 
+            new JacksonJsonRedisSerializer<>(Object.class);
+
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofSeconds(30))         // cache expires every 30 seconds
+                .entryTtl(Duration.ofSeconds(30))
                 .serializeKeysWith(
                     RedisSerializationContext.SerializationPair
                         .fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(
                     RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                        .fromSerializer(serializer));
 
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(config)
@@ -39,10 +42,13 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        JacksonJsonRedisSerializer<Object> serializer = 
+            new JacksonJsonRedisSerializer<>(Object.class);
+
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(serializer);
         return template;
     }
 
